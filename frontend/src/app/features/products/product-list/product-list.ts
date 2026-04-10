46d0c936c5c65b357e6c13a2166598eabb14c071
Joao Leonardo Macanhao <joao.macanhao@ufms.br>
2026-04-10 15:58:42 -0300

    Tela de produtos criada (sem conteudo)

---
+++ frontend/src/app/features/products/product-list/product-list.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  template: `<p>📦 Tela de Produtos (em desenvolvimento)</p>`
+})
+export class ProductListComponent {}
